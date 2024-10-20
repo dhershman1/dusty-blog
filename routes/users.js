aa93cb1d8db964cf5a729a7edb38fe8cbd0a4e38
dhershman1 <dustinh17@gmail.com>
2024-10-20 12:46:07 -0400

    Added: Some documentation to routes

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,17 @@ const router = express.Router()
 
 // Routes for user interactions like registering and logging in/out
 router.post('/register', async (req, res) => {
+  /**
+   * Validates the username and password in the request body and inserts a new user into the database.
+   * Redirects the user to the home page after successful registration.
+   * @param {Object} req - The request object.
+   * @param {Object} req.db - The database connection.
+   * @param {Object} req.body - The body of the request.
+   * @param {string} req.body.username - The username to validate and insert into the database.
+   * @param {string} req.body.password - The password to validate and insert into the database.
+   * @param {Object} res - The response object.
+   * @returns {Promise<void>} A promise that resolves when the user is successfully registered.
+   */
   const usernameRegex = /^[a-zA-Z0-9_-]+$/
   const passwordRegex = process.env.NODE_ENV === 'production'
     ? /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/
@@ -33,6 +44,15 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
+  /**
+   * Retrieves a user from the database based on the provided username in the request body.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} req.db - The database connection.
+   * @param {Object} req.body - The body of the request.
+   * @param {string} req.body.username - The username to search for in the database.
+   * @returns {Promise<Object|null>} A promise that resolves to the found user object or null if no user is found.
+   */
   const foundUser = await req.db('users').select().where('username', req.body.username).first()
 
   if (foundUser && await bcrypt.compare(req.body.password, foundUser.password)) {
@@ -46,6 +66,9 @@ router.post('/login', async (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
+  /**
+   * Destroys the session and redirects the user to the home page.
+   */
   req.session.destroy()
 
   res.redirect('/')
