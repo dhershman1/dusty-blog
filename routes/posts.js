db0e8596c9b8a8d21d8f0f6720302cb3bdd8249a
dhershman1 <dustinh17@gmail.com>
2024-10-19 22:49:51 -0400

    Added: Support for PUT requests and updating posts Fixed: Delete requests not working properly

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ router.get('/:postId', async (req, res) => {
   if (req.headers['content-type'] === 'application/json') {
     res.json(foundPost)
   } else if (foundPost) {
-    res.render('pages/post', { post: foundPost })
+    res.render('pages/post', { post: foundPost, user: req.session.user, userId: req.session.userId })
   } else {
     res.status(404).send('Post not found')
   }
@@ -20,20 +20,48 @@ router.get('/', async (req, res) => {
   res.json(posts)
 })
 
-router.delete('/:postId', (req, res) => {
-  req.db('posts').delete(req.params.postId)
+router.delete('/:postId', async (req, res) => {
+  try {
+    const record = await req.db('posts').where({
+      id: req.params.postId,
+      created_by: req.session.userId
+    }).del(['title'])
 
-  res.render('pages/post-deleted')
+    console.log(record)
+
+    res.render('pages/post-deleted', { user: req.session.user, title: record[0].title })
+  } catch (error) {
+    console.error(error)
+    res.status(401).send('Unauthorized')
+  }
+})
+
+router.put('/:postId', async (req, res) => {
+  try {
+    const foundPost = await req.db('posts').where({
+      id: req.params.postId,
+      created_by: req.session.userId
+    }).update({
+      title: req.body.title,
+      content: req.body.content
+    }, ['title', 'content', 'author', 'created_by'])
+
+    res.render('pages/post', { post: foundPost, user: req.session.user, userId: req.session.userId })
+  } catch (error) {
+    console.error(error)
+    res.status(401).send('Unauthorized')
+  }
 })
 
 router.post('/', async (req, res) => {
   await req.db('posts').insert({
     title: req.body.title,
     content: req.body.content,
-    author: req.body.author || 'Anonymous'
+    author: req.session.user || 'Anonymous',
+    created_by: req.session.userId
   })
 
-  res.render('pages/index')
+  res.render('pages/index', { user: req.session.user })
 })
 
 export default router
