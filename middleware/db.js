cd27dbfe443936e96a7258be4a5154b1471d834a
dhershman1 <dustinh17@gmail.com>
2024-10-20 02:51:55 -0400

    Updated: Unused variables renamed to _

--- middleware/db.js
+++ middleware/db.js
@@ -3,7 +3,7 @@ import knex from 'knex'
 const dbMiddleware = (options) => {
   const db = knex(options)
 
-  return (req, res, next) => {
+  return (req, _, next) => {
     req.db = db
     next()
   }
