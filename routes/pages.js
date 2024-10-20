e75eb2d1c0ab1bba337cb88a10175bf40e1b9ce5
dhershman1 <dustinh17@gmail.com>
2024-10-19 22:49:19 -0400

    Added: Edit page Updated: all renders to pass user session

--- routes/pages.js
+++ routes/pages.js
@@ -3,20 +3,38 @@ import express from 'express'
 const router = express.Router()
 
 // Routes for basic pages like About and Contact
-router.get('/about', (_, res) => {
-  res.render('pages/about')
+router.get('/about', (req, res) => {
+  res.render('pages/about', { user: req.session.user })
 })
 
-router.get('/contact', (_, res) => {
-  res.render('pages/contact')
+router.get('/contact', (req, res) => {
+  res.render('pages/contact', { user: req.session.user })
 })
 
 router.get('/register', (req, res) => {
   if (req.session.user) {
+    return res.redirect('/')
+  }
+
+  res.render('pages/register', { user: req.session.user })
+})
+
+router.get('/login', (req, res) => {
+  if (req.session.user) {
     res.redirect('/')
   }
 
-  res.render('pages/register')
+  res.render('pages/login', { user: req.session.user })
+})
+
+router.get('/edit/:postId', async (req, res) => {
+  const foundPost = await req.db('posts').select().where('id', req.params.postId).first()
+
+  if (req.session.userId === foundPost.created_by) {
+    res.render('pages/edit', { post: foundPost, user: req.session.user, userId: req.session.userId })
+  } else {
+    res.status(401).send('Unauthorized')
+  }
 })
 
 export default router
